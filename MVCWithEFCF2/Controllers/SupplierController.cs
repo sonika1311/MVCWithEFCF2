717bf559cf7cee535da3dd8679fcb34cf595832c
sonika1311 <sonikakumaribom@gmail.com>
2024-08-08 19:08:59 +0530

    removing data in supplier table so that seed method can be implemented

--- MVCWithEFCF2/Controllers/SupplierController.cs
+++ MVCWithEFCF2/Controllers/SupplierController.cs
@@ -13,12 +13,16 @@ namespace MVCWithEFCF2.Controllers
         CompanyDBContext dc = new CompanyDBContext();
         public ActionResult Index()
         {
-            Supplier s1 = new Supplier { Sid = 101, SupplierName = "Ashok Distributors." };
-            Supplier s2 = new Supplier { Sid = 102, SupplierName = "Meghna Distributors." };
-            Supplier s3 = new Supplier { Sid = 103, SupplierName = "Diamond Distributors." };
+            //Supplier s1 = new Supplier { Sid = 101, SupplierName = "Ashok Distributors." };
+            //Supplier s2 = new Supplier { Sid = 102, SupplierName = "Meghna Distributors." };
+            //Supplier s3 = new Supplier { Sid = 103, SupplierName = "Diamond Distributors." };
             //Supplier s4 = new Supplier { Sid = 104, SupplierName = "Prasad Distributors." };
-            dc.Suppliers.Add(s1); dc.Suppliers.Add(s2);
-            dc.Suppliers.Add(s3); //dc.Suppliers.Add(s4);
+            //dc.Suppliers.Add(s1); //dc.Suppliers.Add(s2);
+            //dc.Suppliers.Add(s3); //dc.Suppliers.Add(s4);
+            var sup1 = dc.Suppliers.Find(101);
+            dc.Entry(sup1).State = System.Data.Entity.EntityState.Deleted;
+            dc.Entry(dc.Suppliers.Find(102)).State = System.Data.Entity.EntityState.Deleted;
+            dc.Entry(dc.Suppliers.Find(103)).State = System.Data.Entity.EntityState.Deleted;
             dc.SaveChanges();
             return View(dc.Suppliers);
         }

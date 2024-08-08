1976b5b993aa2a14e04166642ade1af19bc3ecd3
sonika1311 <sonikakumaribom@gmail.com>
2024-08-08 16:37:14 +0530

    added model classes

---
+++ MVCWithEFCF2/Models/CompanyDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithEFCF2.Models
+{
+    public class CompanyDBContext : DbContext
+    {
+       public CompanyDBContext() : base("ConStr")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<CompanyDBContext>());
+        }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ MVCWithEFCF2/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCWithEFCF2.Models
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        [Column(TypeName = "Money")]
+        public decimal? Balance { get; set; }
+        [Index] //Setting Index attribute for this column in database.
+        [Required] //Setting not null constraint for this column in database.
+        [MaxLength(50)]
+        [Column("Cname", TypeName = "Varchar")]
+        public string CustomerName { get; set; }
+        [StringLength(1000)]
+        [Column(TypeName = "Varchar")]
+        public string Address { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Custid { get; set; }
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")] //Setting the foreign key column for the table.
+        public Supplier Supplier { get; set; }
+    }
+}

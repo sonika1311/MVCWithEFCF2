c8821bf174f8941cc7cc165fc82223af17c84f48
sonika1311 <sonikakumaribom@gmail.com>
2024-08-08 17:10:21 +0530

    test -  database being dropped and created everytime

--- MVCWithEFCF2/Controllers/SupplierController.cs
+++ MVCWithEFCF2/Controllers/SupplierController.cs
@@ -16,9 +16,9 @@ namespace MVCWithEFCF2.Controllers
             Supplier s1 = new Supplier { Sid = 101, SupplierName = "Ashok Distributors." };
             Supplier s2 = new Supplier { Sid = 102, SupplierName = "Meghna Distributors." };
             Supplier s3 = new Supplier { Sid = 103, SupplierName = "Diamond Distributors." };
-            Supplier s4 = new Supplier { Sid = 104, SupplierName = "Prasad Distributors." };
+            //Supplier s4 = new Supplier { Sid = 104, SupplierName = "Prasad Distributors." };
             dc.Suppliers.Add(s1); dc.Suppliers.Add(s2);
-            dc.Suppliers.Add(s3); dc.Suppliers.Add(s4);
+            dc.Suppliers.Add(s3); //dc.Suppliers.Add(s4);
             dc.SaveChanges();
             return View(dc.Suppliers);
         }
